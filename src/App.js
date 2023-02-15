6b982e4aabecabadeefbd6c916f9be65a99e5627
Mudit Mishra <Mudit.12016298@lpu.in>
2023-02-15 22:47:58 +0530

    enhancement: Added basic landing page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <div className="bg-zinc-900 pt-10 px-10 pb-5">
+    <div className="bg-[#090909] px-10">
       <Routes>
         <Route path="/home" element=<Home/>/>
         <Route path="/" element=<LandingPage/>/>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,24 +1,78 @@
 import React from "react";
-import classes from './LandingPage.module.css'
+import classes from "./LandingPage.module.css";
 import { Link, NavLink } from "react-router-dom";
 
 function LandingPage() {
   return (
-    <div className="container min-w-full">
-      <NavLink className={"flex justify-between items-center w-full py-3 px-5 mb-10 rounded-lg"}>
+    <div className="container min-w-full min-h-[100vh] pt-10">
+      <NavLink
+        className={"flex justify-between items-center w-full py-3 px-5 mb-10"}
+      >
         <div className="text-xl font-medium text-cyan-100">OnThought</div>
         <div>
-          <Link to="/home" className="text-2xl font-medium bg-yellow-300 py-1 px-3 rounded">Check Now</Link>
+          <Link
+            to="/home"
+            className="text-2xl font-medium bg-yellow-300 py-1 px-3 rounded"
+          >
+            Check Now
+          </Link>
         </div>
       </NavLink>
-      <main className="flex flex-col justify-start items-center pt-10 min-h-[80vh]">
-        <div className="flex flex-col justify-start items-start gap-2">
-          <h2 className="font-bold text-yellow-200 text-9xl tracking-tighter">Think.</h2>
-          <h2 className="text-9xl font-bold text-yellow-300 tracking-tighter">Write.</h2>
-          <h2 className="text-9xl font-bold text-yellow-400 tracking-tighter">Repeat.</h2>
+      <main className="flex items-start">
+        <div className="flex w-full h-[86vh] flex-col items-center justify-center border border-[#2c2c2cd1]">
+          <div className="flex flex-col gap-2 leading-none">
+          <h2 className="text-[10rem] font-bold text-yellow-400 tracking-tighter">
+            Think.
+          </h2>
+          <h2 className="sm:text-[10rem] font-bold text-yellow-300 tracking-tighter">
+            Write.
+          </h2>
+          <h2 className="sm:text-[10rem] font-bold text-yellow-200 tracking-tighter">
+            Repeat.
+          </h2>
+          <p className="text-2xl text-[#525252d1] mt-5 max-w-4xl font-medium">
+            <span className="underline underline-offset-4 text-yellow-100">
+              OnThought
+            </span>{" "}
+            is a place for all your thoughts that need your attention
+          </p>
+          </div>
         </div>
-        <div>
-          <p>OnThought: A palce for your thoughts</p>
+        <div className="flex flex-col h-[86vh]">
+          <div className="bg-inherit hover:bg-[#0f0f0fd1] border border-[#2c2c2cd1] border-l-0 p-8 w-[28rem] h-full flex flex-col justify-evenly items-start gap-1">
+            <div className="text-xl text-[#545454] font-medium">
+              <p>
+                Contrary to popular belief, Lorem Ipsum is not simply random
+                text. It has roots in a piece of classical Latin literature from
+                45 BC, making it over 2000 years old.
+              </p>
+            </div>
+            <div>
+              <Link
+                to="/home"
+                className="text-2xl font-medium bg-yellow-300 py-1 px-3 rounded"
+              >
+                Add Note
+              </Link>
+            </div>
+          </div>
+          <div className="bg-inherit hover:bg-[#0f0f0fd1] border border-t-0 border-l-0 border-[#2c2c2cd1] p-8 w-[28rem] h-full flex flex-col justify-evenly items-start gap-1">
+            <div className="text-xl text-[#545454] font-medium">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                ante risus, mollis non enim id, fermentum eleifend metus. Mauris
+                tincidunt dui quis eros convallis scelerisque
+              </p>
+            </div>
+            <div>
+              <Link
+                to="/home"
+                className="text-2xl font-medium bg-yellow-300 py-1 px-3 rounded"
+              >
+                Add Note
+              </Link>
+            </div>
+          </div>
         </div>
       </main>
     </div>
